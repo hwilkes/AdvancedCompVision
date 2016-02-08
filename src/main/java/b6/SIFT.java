3d4a7d00f93c735de053b45cf4e5d64750109ff4
hwilkes <henry_wilkes@hotmail.co.uk>
2016-02-08 19:58:59 +0000

    Added code to generate KMeans cluster from sift of training images

--- src/main/java/b6/SIFT.java
+++ src/main/java/b6/SIFT.java
@@ -1,26 +1,67 @@
 package b6;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
+import org.openimaj.data.DataSource;
+import org.openimaj.feature.ArrayFeatureVector;
+import org.openimaj.feature.ByteFV;
+import org.openimaj.feature.FeatureExtractor;
+import org.openimaj.feature.SparseIntFV;
+import org.openimaj.feature.local.data.LocalFeatureListDataSource;
 import org.openimaj.feature.local.list.LocalFeatureList;
 import org.openimaj.image.FImage;
+import org.openimaj.image.feature.dense.gradient.dsift.ByteDSIFTKeypoint;
+import org.openimaj.image.feature.dense.gradient.dsift.DenseSIFT;
+import org.openimaj.image.feature.dense.gradient.dsift.PyramidDenseSIFT;
+import org.openimaj.image.feature.local.aggregate.BagOfVisualWords;
+import org.openimaj.image.feature.local.aggregate.BlockSpatialAggregator;
+import org.openimaj.image.feature.local.aggregate.VectorAggregator;
 import org.openimaj.image.feature.local.engine.DoGSIFTEngine;
 import org.openimaj.image.feature.local.keypoints.Keypoint;
+import org.openimaj.ml.clustering.ByteCentroidsResult;
+import org.openimaj.ml.clustering.assignment.HardAssigner;
+import org.openimaj.ml.clustering.kmeans.ByteKMeans;
+import org.openimaj.util.pair.IntFloatPair;
 
 public class SIFT {
 	DoGSIFTEngine engine;
 	
 	private String[] classes;
+	private boolean trained = false;
+	private ByteCentroidsResult clusters;
 	
 	
 	public SIFT(){
 		engine = new DoGSIFTEngine();	
 	}
-	
-	public void trainImages(String[] classes, Map<String,FImage> image){
+	/*
+	 * Makes some use of code snippets from http://www.openimaj.org/tutorial/classification101.html
+	 * */
+	public void trainImages(String[] classes, Map<String,FImage> images){
 		this.classes = classes;
 		
-		//LocalFeatureList<Keypoint> queryKeypoints = engine.findFeatures(query.flatten());
+		Set<FImage> imageSet = new HashSet<FImage>();
+		for(Entry<String,FImage> e : images.entrySet()){
+			imageSet.add(e.getValue());
+		}
+		List<LocalFeatureList<Keypoint>> featurevectors = new ArrayList<LocalFeatureList<Keypoint>>();
+		
+		for(Entry<String,FImage> e : images.entrySet()){
+			LocalFeatureList<Keypoint> keypoints =  engine.findFeatures(e.getValue());
+			featurevectors.add(keypoints);
+		}
+		
+		int means = 500;
+		
+		ByteKMeans km = ByteKMeans.createKDTreeEnsemble(means);
+		DataSource<byte[]> datasource = new LocalFeatureListDataSource<Keypoint, byte[]>(featurevectors);
+		clusters = km.cluster(datasource);
+		trained = true;
 	}
 	
 	public String classify(FImage image){
@@ -28,4 +69,6 @@ public class SIFT {
 		LocalFeatureList<Keypoint> imageKeypoints = engine.findFeatures(image);
 		return null;
 	}
+	
+	
 }
--- src/main/java/b6/SiftSurfComparison.java
+++ src/main/java/b6/SiftSurfComparison.java
@@ -2,17 +2,28 @@ package b6;
 
 import java.io.File;
 import java.io.FilenameFilter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
+import org.openimaj.image.FImage;
+import org.openimaj.image.ImageUtilities;
+import org.openimaj.ml.annotation.Annotated;
+import org.openimaj.ml.annotation.AnnotatedObject;
+
 
 public class SiftSurfComparison {
 	
 	public static void main(String args[]){
-		SiftSurfComparison ssc = new SiftSurfComparison();
+		try {
+			SiftSurfComparison ssc = new SiftSurfComparison();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	//TODO better file organisation
@@ -20,19 +31,19 @@ public class SiftSurfComparison {
 	
 	private final float traningToTestRatio = 0.8f;
 	
-	private File[] testImages;
-	private Map<File,String> trainingImages;
+	private FImage[] testImages;
+	private List<Annotated<FImage,String>> trainingImages;
 	
 	private String[] classes;
 	
 	private Random r;
 	
-	public SiftSurfComparison(){
+	public SiftSurfComparison() throws IOException{
 		List<String> cls = new ArrayList<String>();
-		List<File> ti = new ArrayList<File>();
+		List<FImage> ti = new ArrayList<FImage>();
 		
 		r = new Random();
-		trainingImages = new HashMap<File,String>();
+		trainingImages = new ArrayList<Annotated<FImage,String>>();
 		
 		File dir = new File(imageDir);
 		
@@ -55,21 +66,27 @@ public class SiftSurfComparison {
 		
 		for(File f : classes){
 			if(f.isDirectory()){
-				File[] images = f.listFiles();
+				File[] imageFiles = f.listFiles();
+				FImage[] images = new FImage[imageFiles.length];
+				
+				for(int i = 0; i < imageFiles.length; i++){
+					images[i] = ImageUtilities.readF(imageFiles[i]); 
+				}
+				
 				String c = f.getName();
-				for(File i : images){
+				for(FImage i : images){
 					if(testNum != 0 && r.nextBoolean()){
 						ti.add(i);
 						testNum--;
 					}
 					else{
-						trainingImages.put(i, c);
+						trainingImages.add(new AnnotatedObject<FImage,String>(i,c));
 					}
 				}
 			}
 		}
 		
-		this.testImages = ti.toArray(new File[ti.size()]);
+		this.testImages = ti.toArray(new FImage[ti.size()]);
 		this.classes = cls.toArray(new String[cls.size()]);
 		
 	}
